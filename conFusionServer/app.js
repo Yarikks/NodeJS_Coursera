7fd89ed3bd3386ccc373a358f84ba25613eb2d07
Yarikks <jasper18poil@gmail.com>
2020-06-11 21:04:07 +0300

    require cors

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -8,6 +8,7 @@ var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -41,10 +42,12 @@ app.all('*', (req, res, next) => {
   }
 });
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- conFusionServer/routes/cors.js
+++ conFusionServer/routes/cors.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 
 const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200'];
+
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
